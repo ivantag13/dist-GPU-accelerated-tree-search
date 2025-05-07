c44bc7c9731d87abf27ce5bc075d8b04d815489f
Ivan Tagliaferro <ivantagliaferroot@gmail.com>
2025-05-07 10:16:31 +0200

    Removed unnecessary prints

--- baselines/pfsp/pfsp_dist_multigpu_cuda.c
+++ baselines/pfsp/pfsp_dist_multigpu_cuda.c
@@ -687,8 +687,8 @@ void pfsp_search(const int inst, const int lb, const int m, const int M, const i
 
           // Each process sends its sharedSize to all other processes
           MPI_Allgather(&sharedSize, 1, MPI_INT, sendCounts, 1, MPI_INT, MPI_COMM_WORLD);
-          if (counter % 100 == 0)
-            printf("Proc[%d] sharedSize = %d at counter[%d]\n", MPIRank, sharedSize, counter);
+          // if (counter % 100 == 0)
+          //   printf("Proc[%d] sharedSize = %d at counter[%d]\n", MPIRank, sharedSize, counter);
 
           // Step 4: Calculate displacements for the received data
           int totalReceived = 0;
@@ -698,8 +698,8 @@ void pfsp_search(const int inst, const int lb, const int m, const int M, const i
             recvDispls[i] = totalReceived;
             totalReceived += recvCounts[i];
           }
-          if (counter % 100 == 0)
-            printf("Proc[%d] totalReceived = %d at counter[%d]\n", MPIRank, totalReceived, counter);
+          // if (counter % 100 == 0)
+          //   printf("Proc[%d] totalReceived = %d at counter[%d]\n", MPIRank, totalReceived, counter);
 
           // Step 5: Allocate a buffer to store all received shared data
           Node *receivedNodes = (Node *)malloc(totalReceived * sizeof(Node));
@@ -734,8 +734,8 @@ void pfsp_search(const int inst, const int lb, const int m, const int M, const i
             added++;
           }
 
-          if (counter % 100 == 0)
-            printf("Proc[%d] added = %d at counter[%d]\n", MPIRank, added, counter);
+          // if (counter % 100 == 0)
+          //   printf("Proc[%d] added = %d at counter[%d]\n", MPIRank, added, counter);
 
           pushBackBulk(&multiPool[0], insertNodes, added);
 
