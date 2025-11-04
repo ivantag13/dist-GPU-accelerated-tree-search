6396e27ee762edde370c20ed3c2c773b40686d9f
Ivan Tagliaferro <ivantagliaferroot@gmail.com>
2025-11-04 16:06:36 +0100

    Update LUMI config file

--- config/lumi-module-load.sh
+++ config/lumi-module-load.sh
@@ -2,4 +2,5 @@
 module load LUMI/24.03 partition/G buildtools/24.03
 module load rocm/6.0.3
 module load cray-mpich/8.1.29
+module load PrgEnv-gnu
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/rocm-6.0.3/lib/llvm/lib
